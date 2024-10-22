8cf6c7c88d7201601aa516ab94ee6acab1bfb277
xx8o3ii <fanchan0627@gmail.com>
2024-10-22 09:39:36 +0800

    module&component筆記

--- Desktop/test1020/src/app/app.component.ts
+++ Desktop/test1020/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // 選擇器名稱，可以在別的組件引用
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
@@ -8,11 +9,14 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'test1021';
   taro='1125'
+  // TypeScript 的型別註解，非強制性，但建議要加 ex:string
+  // 使用 $event 的事件處理函式通常需要型別註解來明確指出事件的資料結構
+  // TypeScript 不會自動推斷 any 型別，尤其在嚴格模式下，需要手動指定。
   shibashit($event:any){
     console.log($event);
-
   }
   number = 5;
+  //this 關鍵字用於引用當前物件的屬性或方法。當你需要在類別內部的某個方法中修改物件的屬性（如 number）時，必須使用 this 來明確指定你想修改的是物件的屬性而不是局部變數。
   plus(){
     this.number = this.number+1
   }
--- Desktop/test1020/src/app/app.module.ts
+++ Desktop/test1020/src/app/app.module.ts
@@ -6,16 +6,22 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';  // 確認匯入 FormsModule
 
 @NgModule({
+  // 與畫面相關的程式 只能存在於一個模組 ex:component,directive(用來修改 DOM 的結構或行為 ex:ngFor),pipe(改變資料的顯示形式 ex:date)
   declarations: [
     AppComponent
   ],
+  // 代表要使用哪些模組所提供的功能
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule
 
   ],
+  //要在B模組使用A模組某個元件
+  exports:[],
+  //用來決定哪些service允許被注入 ex:UserService 來處理有關用戶的 CRUD 操作（創建、讀取、更新、刪除）。
   providers: [],
+  //用於定義啟動應用程式的主要根元件
   bootstrap: [AppComponent]
 })
 export class AppModule { }
